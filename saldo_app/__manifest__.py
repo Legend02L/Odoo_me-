8e2f59a4e4250580abc01865af2a9c9b6189a9e8
Legend02L <127001264+Legend02L@users.noreply.github.com>
2023-04-11 15:59:35 -0400

    Add files via upload

---
+++ saldo_app/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "Saldo APP",
+    "description": "Aplicacion WEB para registrar ingresos y egresos",
+    "author": "Luis C.",
+    "version": "1.0",
+    "depends": ["base"],
+    "data":[
+        "security/ir_model_access.xml",
+        "views/views.xml"
+    ]
+}
