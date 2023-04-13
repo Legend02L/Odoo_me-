4c2026df9f50d67dfa5309587cbd991c141c0dda
LUIS ALBERTO CORONADO CANO <luis.coronado@ikeasi.com>
2023-04-13 16:12:08 -0400

    Update de progreso en saldo_app

--- saldo_app/__manifest__.py
+++ saldo_app/__manifest__.py
@@ -3,7 +3,7 @@
     "description": "Aplicacion WEB para registrar ingresos y egresos",
     "author": "Luis C.",
     "version": "1.0",
-    "depends": ["base"],
+    "depends": ["base", "mail"],
     "data":[
         "security/ir_model_access.xml",
         "views/views.xml"
--- saldo_app/models/models.py
+++ saldo_app/models/models.py
@@ -4,11 +4,13 @@ from odoo import fields, models
 class Movimientos(models.Model):
     _name = "sa.movimiento"  # sa_movimiento
     _description = "Movimiento"
+    _inherit = "mail.thread"
 
     name = fields.Char(string="Nombre")
-    type_move = fields.Selection(selection=[("ingreso", "Ingreso"), ("gasto", "Gasto")], string="Tipo", default="ingreso", required=True)
-    date = fields.Datetime(string="Fecha")
-    amount = fields.Float("Monto")
+    type_move = fields.Selection(selection=[("ingreso", "Ingreso"), ("gasto", "Gasto")], track_visibility="onchange",
+                                 string="Tipo", default="ingreso", required=True)
+    date = fields.Datetime(string="Fecha", track_visibility="onchange")
+    amount = fields.Float("Monto", track_visibility="onchange")
     receipt_image = fields.Binary("Foto del recibo")
     notas = fields.Html("Notas")
 
@@ -17,7 +19,7 @@ class Movimientos(models.Model):
     user_id = fields.Many2one("res.users", string="Usuario")
     category_id = fields.Many2one("sa.category", "Categoria")
 
-    tag_id = fields.Many2many("sa.tag", "sa_mov_sa_tag_rel", "move_id", "tag_id")  # sa_movimiento_sa_tag_rel
+    tag_ids = fields.Many2many("sa.tag", "sa_mov_sa_tag_rel", "move_id", "tag_id")  # sa_movimiento_sa_tag_rel
 
 
 class Category(models.Model):
